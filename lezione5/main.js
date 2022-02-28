121ae89d38893d6da8ccb134b02a1c0a8bb2b2de
Sergio Pugliese <pugliese.sergio1987@hotmail.it>
2022-02-28 16:16:15 +0100

    fine lezione

--- lezione5/main.js
+++ lezione5/main.js
@@ -1,1 +1,19 @@
-//main.js
+let caricamento = document.getElementById("caricamento")
+let container = document.getElementById("container")
+let navbar = document.getElementById("navbar")
+let titolo = document.getElementById("titolo")
+let immagine = document.getElementById("immagine")
+immagine.style.display = "none"
+let i = 0
+
+let progress = setInterval(function () {
+    if (i <= 100) {
+        caricamento.style = "width:" + i + "%"
+        titolo.innerText = "Caricamento al " + i + "%"
+        i++
+    } else {
+        clearInterval(progress)   
+        immagine.style.display ="block"
+        immagine.style.textAlign = "center"
+    }
+}, 100);
